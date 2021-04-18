56421a9241f9ecdddee576abaf9f7c6ff52c69e7
Emreturkm <emre.turkmen.123@hotmail.com>
2021-04-19 00:06:20 +0300

    Add project files.

---
+++ Forms/SaticiMenuFrm.cs
@@ -0,0 +1,111 @@
+﻿using PlanlamaOyunu.Entitys;
+using PlanlamaOyunu.SqlQuerys;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanlamaOyunu.Forms
+{
+    public partial class SaticiMenuFrm : Form
+    {
+        public SaticiMenuFrm()
+        {
+            InitializeComponent();
+        }
+        SaticiSorgulari saticiSorgulari = new SaticiSorgulari();
+        List<Urun> urnlr = new List<Urun>();
+
+        private void SaticiMenuFrm_Load(object sender, EventArgs e)
+        {
+            dtGrdViewYeniUrun.ColumnCount = 5;
+            dtGrdViewYeniUrun.Columns[0].Name = "Ürün ID";
+            dtGrdViewYeniUrun.Columns[1].Name = "Ürün adı";
+            dtGrdViewYeniUrun.Columns[2].Name = "Ürün KG";
+            dtGrdViewYeniUrun.Columns[3].Name = "Ürün Kg Fiyatı(TL)";
+            dtGrdViewYeniUrun.Columns[4].Name = "Ürün Onayı";
+
+            dtGrdViewUrunlerim.ColumnCount = 5;
+            dtGrdViewUrunlerim.Columns[0].Name = "Ürün ID";
+            dtGrdViewUrunlerim.Columns[1].Name = "Ürün adı";
+            dtGrdViewUrunlerim.Columns[2].Name = "Ürün KG";
+            dtGrdViewUrunlerim.Columns[3].Name = "Ürün Kg Fiyatı(TL)";
+            dtGrdViewUrunlerim.Columns[4].Name = "Ürün Onayı";
+
+            urnlr = saticiSorgulari.urunlerim();
+            dataGridViewYeniUrunListele(urnlr);
+            urnlr.Clear();
+            urnlr = saticiSorgulari.onayliUrunlerim();
+            dataGridViewUrunlerimListele(urnlr);
+        }
+
+        public void dataGridViewYeniUrunListele(List<Urun> urnlr)
+        {
+            dtGrdViewYeniUrun.Rows.Clear();
+            for (int i = 0; i < urnlr.Count; i++)
+            {
+                dtGrdViewYeniUrun.Rows.Add();
+                dtGrdViewYeniUrun.Rows[i].Cells[0].Value = urnlr[i].urunID;
+                dtGrdViewYeniUrun.Rows[i].Cells[1].Value = urnlr[i].urunAdi;
+                dtGrdViewYeniUrun.Rows[i].Cells[2].Value = urnlr[i].urunKg;
+                dtGrdViewYeniUrun.Rows[i].Cells[3].Value = urnlr[i].urunFiyati;
+                dtGrdViewYeniUrun.Rows[i].Cells[4].Value = urnlr[i].urunOnay;
+            }
+        }
+
+        public void dataGridViewUrunlerimListele(List<Urun> urnlr)
+        {
+            dtGrdViewUrunlerim.Rows.Clear();
+            for (int i = 0; i < urnlr.Count; i++)
+            {
+                dtGrdViewUrunlerim.Rows.Add();
+                dtGrdViewUrunlerim.Rows[i].Cells[0].Value = urnlr[i].urunID;
+                dtGrdViewUrunlerim.Rows[i].Cells[1].Value = urnlr[i].urunAdi;
+                dtGrdViewUrunlerim.Rows[i].Cells[2].Value = urnlr[i].urunKg;
+                dtGrdViewUrunlerim.Rows[i].Cells[3].Value = urnlr[i].urunFiyati;
+                dtGrdViewUrunlerim.Rows[i].Cells[4].Value = urnlr[i].urunOnay;
+            }
+        }
+
+
+
+        private void btnUrunEkle_Click(object sender, EventArgs e)
+        {
+            if (cmbBoxUrunIsmi.Text == "")
+            {
+                MessageBox.Show("Lütfen boş kutucuk bırakmayınız!");
+            }
+            else
+            {
+                if (txtBoxUrunIsmi.Visible == true)
+                {
+                    saticiSorgulari.YeniUrunSatici(txtBoxUrunIsmi.Text, Convert.ToInt32(txtBoxUrunKilo.Text), Convert.ToDouble(txtBoxUrunKgFiyat.Text));
+                }
+                else
+                {
+                    saticiSorgulari.YeniUrunSatici(cmbBoxUrunIsmi.Text, Convert.ToInt32(txtBoxUrunKilo.Text), Convert.ToDouble(txtBoxUrunKgFiyat.Text));
+                }
+                urnlr.Clear();
+                urnlr = saticiSorgulari.urunlerim();
+                dataGridViewYeniUrunListele(urnlr);
+            }
+        }
+
+        private void cmbBoxUrunIsmi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbBoxUrunIsmi.Text == "Diğer")
+            {
+                txtBoxUrunIsmi.Visible = true;
+            }
+            else
+            {
+                txtBoxUrunIsmi.Visible = false;
+            }
+        }
+    }
+}
---
+++ SqlQuerys/SaticiSorgulari.cs
@@ -0,0 +1,90 @@
+﻿using PlanlamaOyunu.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanlamaOyunu.SqlQuerys
+{
+    public class SaticiSorgulari
+    {
+        public static string connectionSource = Properties.Settings.Default.SqlString;
+        SqlConnection baglanti = new SqlConnection(connectionSource);
+
+        public void YeniUrunSatici(string urunİsmi, int urunKilosu, double kgFiyati)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT tblUrun (kullaniciID,urunAdi,urunKg,urunFiyati,urunOnay) VALUES " + " (@kullaniciID,@urunAdi, @urunKg, @urunFiyati, 0)", baglanti);
+            komut.Parameters.AddWithValue("@urunAdi", urunİsmi);
+            komut.Parameters.AddWithValue("@urunKg", urunKilosu);
+            komut.Parameters.AddWithValue("@urunFiyati", kgFiyati);
+            komut.Parameters.AddWithValue("@kullaniciID", Properties.Settings.Default.kullaniciID);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Başarıyla Oluşturulmuştur.");
+        }
+
+        public List<Urun> urunlerim()
+        {
+            List<Urun> urnlr = new List<Urun>();
+            try
+            {
+                baglanti.Open();//veritabanı ile olan bağlantıyı açıyor
+                SqlCommand komut = new SqlCommand("Select * from tblUrun Where kullaniciID = @kullaniciId", baglanti);//sorgumuz
+                komut.Parameters.AddWithValue("kullaniciId", Properties.Settings.Default.kullaniciID);
+                SqlDataReader read = komut.ExecuteReader();//sorgudan dönen değerleri okuyor
+                while (read.Read())
+                {
+                    Urun urn = new Urun();
+                    urn.urunID = Convert.ToInt32(read["urunID"]);
+                    urn.kullaniciID = Convert.ToInt32(read["kullaniciID"]);
+                    urn.urunAdi = read["urunAdi"].ToString();
+                    urn.urunKg = Convert.ToDouble(read["urunKg"]);
+                    urn.urunFiyati = Convert.ToDouble(read["urunFiyati"]);
+                    urn.urunOnay = Convert.ToBoolean(read["urunOnay"]);
+                    urnlr.Add(urn);
+                }
+                baglanti.Close();
+                return urnlr;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.ToString(), "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        public List<Urun> onayliUrunlerim()
+        {
+            List<Urun> urnlr = new List<Urun>();
+            try
+            {
+                baglanti.Open();//veritabanı ile olan bağlantıyı açıyor
+                SqlCommand komut = new SqlCommand("Select * from tblUrun Where kullaniciID = @kullaniciId and urunOnay = 1", baglanti);//sorgumuz
+                komut.Parameters.AddWithValue("kullaniciId", Properties.Settings.Default.kullaniciID);
+                SqlDataReader read = komut.ExecuteReader();//sorgudan dönen değerleri okuyor
+                while (read.Read())
+                {
+                    Urun urn = new Urun();
+                    urn.urunID = Convert.ToInt32(read["urunID"]);
+                    urn.kullaniciID = Convert.ToInt32(read["kullaniciID"]);
+                    urn.urunAdi = read["urunAdi"].ToString();
+                    urn.urunKg = Convert.ToDouble(read["urunKg"]);
+                    urn.urunFiyati = Convert.ToDouble(read["urunFiyati"]);
+                    urn.urunOnay = Convert.ToBoolean(read["urunOnay"]);
+                    urnlr.Add(urn);
+                }
+                baglanti.Close();
+                return urnlr;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.ToString(), "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+    }
+}
